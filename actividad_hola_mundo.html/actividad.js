03080e1101088934854c5e0231ca70fc23487096
Lorena755 <c.verdugo27@alumnos.santotomas.cl>
2026-04-09 21:44:21 -0400

    Add files via upload

---
+++ actividad_hola_mundo.html/actividad.js
@@ -0,0 +1,11 @@
+// 1. Alerta emergente
+alert("Hola Mundo");
+
+// 2. Mensaje en el documento 
+document.getElementById("mensaje-parrafo").textContent = "Hola Mundo ";
+
+// 3. Impresión en consola 
+console.log("Hola Mundo");
+
+// 4. Mensaje dentro de un <div>
+document.getElementById("mensaje-div").innerHTML = "<strong>Hola Mundo</strong> ";
